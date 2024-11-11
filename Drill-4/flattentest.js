fd35065e30638dd0828527c5be99f859c1a2d104
Murali <murali15062003@gmail.com>
2024-11-11 11:37:08 +0530

    Updating the flattentest.js

--- Drill-4/flattentest.js
+++ Drill-4/flattentest.js
@@ -1,16 +1,15 @@
 
 const nestedArray = [1, [2], [[3]], [[[4]]]]; // use this to test 'flatten'
 import { flatten } from "./flattensolution.js";
-import { items } from "./arrays.js";
 
 
-if (typeof items != "object" && Array.isArray(items)!=true) {
+if (typeof nestedArray != "object" && Array.isArray(nestedArray)!=true) {
     console.error("Invalid Data");
-} else if (typeof items == "undefined") {
+} else if (typeof nestedArray == "undefined") {
     console.error("No Parameters given");
 }
 else {
-    let required = flatten(items);
+    let required = flatten(nestedArray);
     console.log(required);
     
 }
